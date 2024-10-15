385a3f21ac704f4bbaed839d54107b4eeced9519
Anton Zinchenko <12981061+Antoniozinchenko@users.noreply.github.com>
2024-10-15 21:50:43 +0300

    fix jira title

--- commands/jiracommit.js
+++ commands/jiracommit.js
@@ -1,4 +1,5 @@
 import { runCommand } from "./run_command.js";
+import { jiraTitle } from "./jiratitle.js";
 
 // Add all changes, get the Jira title, and commit with that message
 export function jiracommit() {
