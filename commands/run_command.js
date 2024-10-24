0021024dc9ddf5e8ad41032bf5585e2add083891
Anton Zinchenko <12981061+Antoniozinchenko@users.noreply.github.com>
2024-10-24 12:46:22 +0300

    remove trycatch for runCommand

--- commands/run_command.js
+++ commands/run_command.js
@@ -2,9 +2,5 @@ import { execSync } from "child_process";
 
 // Helper function to execute shell commands and get the output
 export function runCommand(command) {
-  try {
-    return execSync(command, { encoding: "utf-8" }).trim();
-  } catch (error) {
-    return null;
-  }
+  return execSync(command, { encoding: "utf-8" }).trim();
 }
