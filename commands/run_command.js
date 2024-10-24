f5b82faee629a0a312b30e426c6cea00ae6baf88
Anton Zinchenko <12981061+Antoniozinchenko@users.noreply.github.com>
2024-10-24 14:28:29 +0300

    add process.exit(1)

--- commands/run_command.js
+++ commands/run_command.js
@@ -2,5 +2,9 @@ import { execSync } from "child_process";
 
 // Helper function to execute shell commands and get the output
 export function runCommand(command) {
-  return execSync(command, { encoding: "utf-8" }).trim();
+  try {
+    return execSync(command, { encoding: "utf-8" }).trim();
+  } catch (error) {
+    process.exit(1);
+  }
 }
